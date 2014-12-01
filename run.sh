9bb55780e58155ebe8a7066b8b90411dfbf0b2ff
Jamie Lennox <jamielennox@redhat.com>
2014-12-01 16:23:28 +1000

    Lots of changes, need to make this public

--- run.sh
+++ run.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
+usage() {
     echo "Enter the SSH string (or a comma seperated list of machines) to the machine to connect to"
     echo ""
     echo "e.g. $0 fedora@127.0.0.1"
+    echo "the -d flag will run devstack after creation"
     exit 1
+}
+
+while getopts ":d" o; do
+    case "${o}" in
+        d)
+            devstack=1
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z $1 ]; then
+    usage
 fi
 
-echo "ansible-playbook -i \"$1,\" devstack.yml"
+# echo "ansible-playbook -i \"$1,\" devstack.yml"
 ansible-playbook -i "$1," devstack.yml
+
+if [ $devstack ]; then
+    ssh $1 "~/devstack/unstack.sh; ~/devstack/stack.sh"
+fi
