f1dd0dde16eada72b9634a59923d76c1428e81be
Jamie Lennox <jamielennox@redhat.com>
2015-07-02 10:07:49 +1000

    Split different types of service

---
+++ create-common.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+usage() {
+    echo "Enter the SSH string (or a comma seperated list of machines) to the machine to connect to"
+    echo ""
+    echo "e.g. $0 --hostname openstack.jamielennox.net fedora@127.0.0.1"
+    exit 1
+}
+
+options=$(getopt -l hostname -- "$@")
+
+if [ ! options ]; then
+    echo "options $options"
+    usage
+    exit 1
+fi
+
+while [ $# -gt 1 ]; do
+    case "$1" in
+        --hostname)
+            hostname=$2
+            shift
+            ;;
+        *)
+            echo "Unknown argument $1"
+            usage
+            ;;
+    esac
+    shift
+done
+
+if [ -z $1 ]; then
+    usage
+fi
+
+if [ $hostname ]; then
+    extra_vars="--extra-vars \"hostname=$hostname\""
+fi
+
+echo ansible-playbook -i \"$1,\" $extra_vars common.yml
+ansible-playbook -i "$1," $extra_vars common.yml
---
+++ create-ipa.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+usage() {
+    echo "Enter the SSH string (or a comma seperated list of machines) to the machine to connect to"
+    echo ""
+    echo "e.g. $0 jamielennox-fedtest.org fedora@127.0.0.1"
+    exit 1
+}
+
+if [ -z $1 -o -z $2 ]; then
+    usage
+fi
+
+echo ansible-playbook -i \"$2,\" --extra-vars \"domain=$1\" ipa-server.yml
+ansible-playbook -i "$2," --extra-vars "domain=$1" ipa-server.yml
